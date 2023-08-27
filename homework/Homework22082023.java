c2e026782b6857a584bbfe647dd6bd6edeff5457
Alex <133807020+alexandrsolomonenko@users.noreply.github.com>
2023-08-27 19:17:38 +0200

    Add files via upload

---
+++ homework/Homework22082023.java
@@ -0,0 +1,63 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Homework22082023 {
+    public static void main(String[] args) {
+
+        // Примитивные типы
+        //int intValue = 10;
+        //byte byteValue = 100;
+        //short shortValue = 500;
+        //long longValue = 100000L;
+        //float floatValue = 3.14f;
+        //double doubleValue = 2.71828;
+        //char charValue = 'A';
+        //boolean booleanValue = true;
+
+        // Не примитивные типы (объекты)
+        //String stringValue = "Привет, мир!";
+        //Integer integerValue = 42;
+        //Double doubleObjectValue = 3.14159;
+        //Character charObjectValue = 8;
+        //Boolean booleanObjectValue = false;
+
+        //Вывести их результат в консоль:
+//        int intValue = 89;
+//        byte byteValue = 4;
+//        short shortValue = 56;
+//        long longValue = 12121L;
+//        float floatValue = 4.7333436f;
+//        double doubleValue = 4.355453532;
+//        char charValue = 'G';
+//
+//        System.out.println("char: " + charValue);
+//        System.out.println("int: " + intValue);
+//        System.out.println("byte: " + byteValue);
+//        System.out.println("short: " + shortValue);
+//        System.out.println("float: " + floatValue);
+//        System.out.println("double: " + doubleValue);
+//        System.out.println("long: " + longValue);
+
+        //Дано трехзначное число. Вывести на экран все цифры этого числа
+        //Пример: 345
+        //Вывод в консоль: Число 345 -> 3, 4, 5
+
+//        int number = 345;
+//
+//        System.out.print("Число " + number + " -> ");
+//
+//        // Получаем первую цифру числа
+//        int firstDigit = number / 100;
+//        System.out.print(firstDigit);
+//
+//        // Получаем вторую цифру числа
+//        int secondDigit = (number / 10) % 10;
+//        System.out.print(", " + secondDigit);
+//
+//        // Получаем третью цифру числа
+//        int thirdDigit = number % 10;
+//        System.out.println(", " + thirdDigit);
+
+
+
+    }
+}
