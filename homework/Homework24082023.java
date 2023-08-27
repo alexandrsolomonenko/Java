2437de789f74531c526517e5416006d30bf2e78c
Alex <133807020+alexandrsolomonenko@users.noreply.github.com>
2023-08-27 19:17:19 +0200

    Add files via upload

---
+++ homework/Homework24082023.java
@@ -0,0 +1,56 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Homework24082023 {
+    public static void main(String[] args) {
+
+//        String str = new String("I study Basic Java!");
+//
+//        int length = str.length();
+//        char preLastChar = str.charAt(length - 2);
+//        System.out.println("Пред-последний символ: " + preLastChar);
+//
+//
+//        boolean containsJava = str.contains("Java");
+//        System.out.println("Содержит подстроку : " + containsJava);
+//
+//
+//        String substring = str.substring(str.indexOf("Java"), str.indexOf("Java") + 4);
+//        System.out.println("Вырезанная строка : " + substring);
+//
+//
+//        String replacedString = str.replace('a', 'o');
+//        System.out.println("Строка после замены символов: " + replacedString);
+//
+//
+//        String upperCaseString = str.toUpperCase();
+//        System.out.println("Строка в верхнем регистре: " + upperCaseString);
+//
+//
+//        String lowerCaseString = str.toLowerCase();
+//        System.out.println("Строка в нижнем регистре: " + lowerCaseString);
+
+//        Создайте программу, в которой нужно ввести 2 параметра, сделайте  операции с математическими операциями +, -, *, /
+//        Результат распечатайте в консоль
+
+
+//        Scanner scanner = new Scanner(System.in);
+//
+//        System.out.print("Введите первое число: ");
+//        double number1 = scanner.nextDouble();
+//
+//        System.out.print("Введите второе число: ");
+//        double number2 = scanner.nextDouble();
+//
+//        double sum = number1 + number2;
+//        double difference = number1 - number2;
+//        double product = number1 * number2;
+//        double quotient = number1 / number2;
+//
+//        System.out.println("+: " + sum);
+//        System.out.println("-: " + difference);
+//        System.out.println("*: " + product);
+//        System.out.println("/: " + quotient);
+
+
+        }
+    }
